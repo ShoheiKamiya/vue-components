b9acde8607e564bdafbde6c1553baff6106430a7
ShoheiKamiya <shoooooo.bqt@gmail.com>
2020-09-19 00:35:48 +0900

    hello prettier (using eslint-plugin-prettier)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,20 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2017": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/recommended",
-        "standard",
-        "prettier"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 2019,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue",
-    ],
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es2017: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/recommended",
+    "standard",
+    "prettier"
+  ],
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: "module"
+  },
+  plugins: ["vue", "prettier"],
+  rules: {
+    "prettier/prettier": "error"
+  }
 };
