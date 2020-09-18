ff97fa90671f20473f03b3f81de243163295d615
ShoheiKamiya <shoooooo.bqt@gmail.com>
2020-09-19 00:20:29 +0900

    hello eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2017": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/recommended",
+        "standard",
+        "prettier"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 2019,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue",
+    ],
+    "rules": {
+    }
+};
