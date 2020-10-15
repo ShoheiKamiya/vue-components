04711793e2a666d8527ff067b43fe0a2eb941f75
ShoheiKamiya <49185403+ShoheiKamiya@users.noreply.github.com>
2020-10-15 23:13:47 +0900

    release (#11)
    * 03 charts (#10)
    
    * init 03-charts
    
    * install chart.js
    
    * add bar chart
    
    * build

---
+++ src/projects/03-charts/BarChart.vue
@@ -0,0 +1,54 @@
+<script>
+import { Bar } from 'vue-chartjs';
+
+export default {
+  extends: Bar,
+  props: {
+    chartdata: {
+      type: Object,
+      required: true,
+    },
+  },
+  data: () => ({
+    options: {
+      responsive: true,
+      maintainAspectRatio: true,
+      tooltips: {
+        enabled: false,
+      },
+      scales: {
+        xAxes: [
+          {
+            gridLines: {
+              drawOnChartArea: false,
+              drawTicks: false,
+            },
+            ticks: {
+              padding: 5,
+              stepSize: 5,
+            },
+          },
+        ],
+        yAxes: [
+          {
+            ticks: {
+              min: 0,
+              max: 10,
+              padding: 5,
+              stepSize: 5,
+            },
+            gridLines: {
+              drawOnChartArea: false,
+              drawTicks: false,
+            },
+          },
+        ],
+      },
+    },
+  }),
+
+  mounted() {
+    this.renderChart(this.chartdata, this.options);
+  },
+};
+</script>
