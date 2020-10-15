692fbb5e82cfb32c3998ee0d9a1f53616ec3abe9
ShoheiKamiya <shoooooo.bqt@gmail.com>
2020-10-15 22:53:28 +0900

    add bar chart

---
+++ src/projects/03-charts/data.js
@@ -0,0 +1,30 @@
+export const chartdata = {
+  labels: ['10/13', '10/14', '10/15'],
+  datasets: [
+    {
+      label: 'ルスツ',
+      backgroundColor: '#feca57',
+      data: [10, null, 30],
+    },
+    {
+      label: 'テイネ',
+      backgroundColor: '#ff9f43',
+      data: [10, null, 5],
+    },
+    {
+      label: 'ヒラフ',
+      backgroundColor: '#ff6b6b',
+      data: [5, 5, 20],
+    },
+    {
+      label: 'ヴィレッジ',
+      backgroundColor: '#48dbfb',
+      data: [20, 0.1, 0.1],
+    },
+    {
+      label: 'トマム',
+      backgroundColor: '#1dd1a1',
+      data: [0.1, 0.1, 5],
+    },
+  ],
+};
--- src/views/ChartsPage.vue
+++ src/views/ChartsPage.vue
@@ -1,5 +1,33 @@
 <template>
-  <div>
-    charts page
+  <div class="container">
+    <BarChart :chartdata="chartdata" class="bar-chart" />
   </div>
 </template>
+
+<script>
+import BarChart from '@/projects/03-charts/BarChart';
+import { chartdata } from '@/projects/03-charts/data.js';
+
+export default {
+  components: {
+    BarChart,
+  },
+  data() {
+    return {
+      chartdata: chartdata,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.container {
+  width: 375px;
+  height: 500px;
+  .bar-chart {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
