fd5f4a0b481156cf36a2cffc0c6756c9cae26577
ShoheiKamiya <shoooooo.bqt@gmail.com>
2021-01-08 14:59:08 +0900

    devServer https

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
+const path= require('path')
+const fs= require('fs')
+
 module.exports = {
   publicPath: '/vue-components/',
   outputDir: 'docs',
+  devServer: {
+    https: process.env.NODE_ENV === 'production' ? {} : {
+      key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem'))
+    }
+  }
 };
